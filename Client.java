c27b621696ff01ffaa5afca43bfb7255b536eee0
FastFahrul <fakhruddin.dahodwala@students.mq.edu.au>
2023-04-01 18:42:09 +1100

    Added working communication loop functionality with lrr scheduling

--- Client.java
+++ Client.java
@@ -1,27 +1,37 @@
 import java.io.BufferedReader;
-import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 
-public class Client{
-    public static void main(String[] args){
-        try{
+import javax.swing.RepaintManager;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
             // create a new socker object that connects to ds-server on localhost port 50000
-            Socket socket = new Socket("localhost",50000);
+            Socket socket = new Socket("localhost", 50000);
 
             // create a new DataOutputStream object to write/send data to the ds-server
             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-            
+
             // create BufferedReader object to read data sent from the ds-server
             BufferedReader dis = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             // contains reponse from ds-server
             String response;
+
+            // Job ID variable
+            int jobID;
+
+            // Server ID variable
+            int serverID;
+
+            // total number of largest server types
+            int totalServers;
+
+            // name of server type
+            String serverType;
 
             // send HELO to ds-server
             dout.write(("HELO\n").getBytes());
@@ -39,23 +49,21 @@ public class Client{
             response = (String) dis.readLine();
             System.out.println("message: " + response);
 
-            // send REDY to ds-server
-            dout.write(("REDY\n").getBytes());
-            dout.flush();
+            while (true) {
+                // send REDY to ds-server
+                dout.write(("REDY\n").getBytes());
+                dout.flush();
 
-            // read and print out message
-            response = (String) dis.readLine();
-            System.out.println("message: " + response);
+                // read and print out message
+                response = (String) dis.readLine();
+                System.out.println("message: " + response);
 
-            int i = 0;
-
-            while(i<1){
-                if(!response.equals("NONE")){
+                while (!response.equals("NONE")) {
                     // string array that contains JOBN command reponse from ds-sever
                     String[] jobnParts = response.split(" ");
                     // index number for Job ID in the JOBN command is 2
-                    String JobID = jobnParts[2];
-                    System.out.println("The Job ID is: " + JobID);
+                    jobID = Integer.parseInt(jobnParts[2]);
+                    System.out.println("The Job ID is: " + jobID);
 
                     // send GETS All to ds-server
                     dout.write(("GETS All\n").getBytes());
@@ -74,39 +82,15 @@ public class Client{
                     dout.flush();
 
                     // arraylist to add array of server info string
-                    ArrayList<String> serverInfo = new ArrayList<String>();
+                    ArrayList<String[]> serverInfo = new ArrayList<String[]>();
 
-                    // print DATA received from ds-server line by line (number of lines provided by limiter)
-                    for(int j=0; j<limiter; j++){
+                    // print DATA received from ds-server line by line (number of lines provided by
+                    // limiter)
+                    for (int j = 0; j < limiter; j++) {
                         response = (String) dis.readLine();
-                        serverInfo.add(response);
+                        serverInfo.add(response.split(" "));
                     }
 
-                    // make a custom comparator to sort items in serverInfo arrayList
-                    Comparator<String> lrrComparator = new Comparator<String>() {
-                        @Override
-                        public int compare(String s1, String s2) {
-                            String[] parts1 = s1.split(" ");
-                            String[] parts2 = s2.split(" ");
-                            int value1 = Integer.parseInt(parts1[4]);
-                            int value2 = Integer.parseInt(parts2[4]);
-                            int result = Integer.compare(value2, value1); // sort by index 4 in descending order
-                            if (result == 0) { // if index 4 values are equal
-                                int index2Value1 = Integer.parseInt(parts1[1]);
-                                int index2Value2 = Integer.parseInt(parts2[1]);
-                                result = Integer.compare(index2Value1, index2Value2); // sort by index 2 in ascending order
-                            }
-                            return result;
-                        }
-                    };
-
-                    // call sort on serverInfo with custom
-                    Collections.sort(serverInfo, lrrComparator);
-
-                    for(String element: serverInfo){
-                        System.out.println(element);
-                    }
-                    
                     // send OK to ds-server
                     dout.write(("OK\n").getBytes());
                     dout.flush();
@@ -115,11 +99,67 @@ public class Client{
                     response = (String) dis.readLine();
                     System.out.println("message: " + response);
 
-                    i++;
-                }
+                    // array list of all the servers with the largest cores
+                    ArrayList<String[]> largestServers = new ArrayList<String[]>();
 
-                else{
-                    // exit loop is NONE received from ds-server
+                    // find largest number of cores in the server list
+                    int maxValue = 0;
+                    for (String[] element : serverInfo) {
+                        int result = Integer.parseInt(element[4]);
+                        if (result > maxValue) {
+                            maxValue = result;
+                        }
+                    }
+
+                    System.out.println("The largest number of cores in a server is: " + maxValue);
+
+                    // add the severs into largestServer arrayList that have the largest number of
+                    // cores
+                    for (String[] element : serverInfo) {
+                        String maxString = String.valueOf(maxValue);
+                        if (element[4].equals(maxString)) {
+                            largestServers.add(element);
+                        }
+                    }
+
+                    // index of largestServers arrayList
+                    int index;
+                    serverType = largestServers.get(0)[0];
+
+                    while (!response.equals("NONE")) {
+                        totalServers = largestServers.size();
+                        index = jobID % totalServers;
+
+                        serverID = Integer.parseInt(largestServers.get(index)[1]);
+                        System.out.println(serverID);
+
+                        // send SCHD to ds-server
+                        dout.write(("SCHD " + String.valueOf(jobID) + " " + serverType + " " + String.valueOf(serverID)
+                                + "\n").getBytes());
+
+                        // read and print out message from ds-server
+                        response = (String) dis.readLine();
+                        System.out.println("message: " + response);
+
+                        dout.write(("REDY\n").getBytes());
+                        dout.flush();
+                        
+                        response = (String) dis.readLine();
+                        System.out.println("message: " + response);
+
+                        String[] tester = response.split(" ");
+
+                        if(tester[0].equals("JOBN")){
+                            jobnParts = response.split(" ");
+                            jobID = Integer.parseInt(jobnParts[2]);
+                            System.out.println("The Job ID is: " + jobID);
+                        }
+                    }
+
+                }
+                
+                // break from loop if reponse is none
+                if (response.equals("NONE")) {
                     break;
                 }
             }
@@ -136,8 +176,8 @@ public class Client{
             dis.close();
             dout.close();
             socket.close();
-        }
-        catch(Exception e){
+
+        } catch (Exception e) {
             // print error message if exception is thrown
             System.out.println(e);
         }
